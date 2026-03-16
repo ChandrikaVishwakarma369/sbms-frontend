bdc3de97d8df66c957a1f7ed55f5ab86916731b9
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-03-16 12:56:10 +0530

    fix: update app routing and package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import Invoices from "./pages/Invoices";
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Invoices from "./pages/Invoices";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Customers from "./pages/Customers";
 
 function App() {
   return (
     <Router>
       <Routes>
-
         {/* Default Route */}
-        <Route path="/" element={<Navigate to="/customers" />} />
-
+        <Route path="/" element={<Dashboard />} />
+        
         {/* Customers Page */}
         <Route path="/customers" element={<Customers />} />
-          
+
+        {/* Invoices Page */}
         <Route path="/invoices" element={<Invoices />} />
       </Routes>
     </Router>
