5d32462860a8af8436e9a02ba1ae8d3b52191615
Prachi Likhar <prachilikhar81@gmail.com>
2026-03-20 10:18:05 +0530

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 import Dashboard from "./pages/Dashboard";
-
+import Settings from "./pages/Settings";
 function App() {
-  return <Dashboard />;
+  return (
+    <>
+      <Dashboard />;
+    </>
+  );
 }
 
 export default App;
