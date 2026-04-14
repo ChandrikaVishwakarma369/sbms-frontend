38c6669105f039154546002adef68d8b06a58d92
Prachi Likhar <prachilikhar81@gmail.com>
2026-04-14 22:53:10 +0530

    Products page + edit modal + API integration

--- src/pages/loginsignup.jsx
+++ src/pages/loginsignup.jsx
@@ -19,10 +19,16 @@ const LoginSignup = () => {
     const password = e.target[1].value;
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true, // 🔥 MOST IMPORTANT
+        }
+      );
 
       console.log(res.data);
 
